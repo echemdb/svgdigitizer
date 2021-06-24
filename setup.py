f80f0a0b141c80c6e2977ee0dbf88343e574232e
Julian Rüth <julian.rueth@fsfe.org>
2021-06-25 00:55:21 +0200

    add setup.py to release on PyPI

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from distutils.core import setup
+
+setup(
+    name = 'svgdigitizer',
+    version = '0.1.0',
+    packages = ['svgdigitizer',],
+    license = 'GPL 3.0+',
+    long_description = open('README.md').read(),
+    include_package_data=True,
+)
