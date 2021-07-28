351188b638ddb2e341994ca63fce65b21032c214
Julian Rüth <julian.rueth@fsfe.org>
2021-07-28 15:48:15 +0200

    Fix lint
    and allow long lines. Nobody is really following that part of the PEP I
    believe.

--- svgdigitizer/__main__.py
+++ svgdigitizer/__main__.py
@@ -1,7 +1,9 @@
 import click
 
+
 @click.group()
 def cli(): pass
+
 
 @click.command()
 @click.option('--sampling_interval', type=float, default=None, help='specify sampling interval (for now in mV)')
@@ -9,6 +11,7 @@ def cli(): pass
 def plot(svg, sampling_interval):
     from svgdigitizer.svgplot import SVGPlot
     SVGPlot(svg, sampling_interval=sampling_interval).plot()
+
 
 @click.command()
 @click.option('--sampling_interval', type=float, default=None, help='specify sampling interval (for now in mV)')
@@ -19,6 +22,7 @@ def digitize(svg, sampling_interval):
     from pathlib import Path
     plot.dfs[0].to_csv(Path(svg).with_suffix('.csv'), index=False)
 
+
 @click.command()
 @click.option('--onlypng', is_flag=True, help='Only produce png files')
 @click.argument('pdf')
@@ -27,24 +31,22 @@ def paginate(onlypng, pdf):
     import svgwrite
     from PIL import Image
     basename = pdf.split('.')[0]
-    pages = convert_from_path(pdf, dpi=600,)
+    pages = convert_from_path(pdf, dpi=600)
     for idx, page in enumerate(pages):
         base_image_path = f'{basename}_p{idx}'
         page.save(f'{base_image_path}.png', 'PNG')
         if not onlypng:
             image = Image.open(f'{base_image_path}.png')
             width, height = image.size
-            dwg = svgwrite.Drawing(f'{base_image_path}.svg', 
-            size = (f'{width}px',
-                    f'{height}px'), profile='tiny')
-            dwg.add(svgwrite.image.Image(f'{base_image_path}.png', insert=(0,0), size = (f'{width}px',
-                    f'{height}px')))
+            dwg = svgwrite.Drawing(f'{base_image_path}.svg', size=(f'{width}px', f'{height}px'), profile='tiny')
+            dwg.add(svgwrite.image.Image(f'{base_image_path}.png', insert=(0, 0), size=(f'{width}px', f'{height}px')))
             dwg.save()
 
 
 cli.add_command(plot)
 cli.add_command(digitize)
 cli.add_command(paginate)
+
 
 if __name__ == "__main__":
     cli()
