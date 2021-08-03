c3884bb8a2071bc1e3fffcb979a2d9c16df811f9
Albert Engstfeld <albert.engstfeld@gmail.com>
2021-08-03 20:38:59 +0200

    Modified CV to use unit strings from svgplot to get potential and time axis
    This version creates a dataframe with a voltage and time axis

--- svgdigitizer/electrochemistry/cv.py
+++ svgdigitizer/electrochemistry/cv.py
@@ -1,6 +1,38 @@
+from svgdigitizer.svgplot import SVGPlot
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
+from astropy import units as u
+
+# The unit strings are identical to those obtained by using str() on an astropy unit of a quantity, i.e., 
+# >>> q = 10 * u.A / u.cm**2
+# >>> str(q.unit)
+# 'A / cm2'
+units = {'uA / cm2': ['uA / cm2',
+                      'uA / cm²',
+                      'µA / cm²',
+                      'µA cm⁻²', 
+                      'uA cm-2', 
+                      'uA / cm2'],
+        'A / cm2': ['A / cm2', 
+                    'A cm⁻²', 
+                    'A cm-2', 
+                    'A / cm2'],
+        'A': ['A',
+                'ampere', 
+                'amps', 
+                'amp'],
+        'mV': ['milliV',
+                'millivolt',
+                'milivolt',
+                'miliv,' 
+                'mV'],
+        'V': ['V', 
+              'v',
+              'Volt',
+              'volt'], 
+        'V / s': ['V s-1',
+                  'V / s']}
 
 
 class CV():
@@ -25,9 +57,26 @@ class CV():
 
         self.yunit = self.description['current']['unit']
 
+        self.axis_units = self.get_axis_units()
+
         self.get_rate()
 
-        self.modify_df()
+        #self.modify_df()
+
+        
+
+    def get_axis_units(self):
+        '''replaces the units derived from the svg file into strings that can be used with astropy'''
+        
+        axis_unit_strings = self.svgplot.get_axis_unit_strings()
+        axis_units ={}
+
+        for idx, i in enumerate(axis_unit_strings):
+            for unit in units:
+                if axis_unit_strings[i] in units[unit]:
+                    axis_units[i] = u.Unit(unit)
+        
+        return axis_units
 
     def get_rate(self):  # TODO: probably not required
         r'''
@@ -38,39 +87,47 @@ class CV():
         self.rate = self.description['scan rate']['value']
         return self.rate
 
-    def modify_df(self):
-        # Create potential columns
-        self.df = self.create_df_U_axis(self.svgplot.dfs[0])
-        # Create current columns
-        self.df = pd.concat(
-            [self.df, self.create_df_I_axis(self.svgplot.dfs[0])],
-            axis=1)
+    def create_cv_df(self):
+        # Create potential column
+        self.df = self.create_df_U_axis(self.svgplot.dfs[0][['x']])
+
+        # Create current or current density column
+        # self.df = pd.concat([self.df, self.create_df_I_axis(self.svgplot.dfs[0][['y']])], axis=1)
 
         # create time axis
-        self.df['t'] = self.create_df_time_axis(self.svgplot.dfs[0])
+        self.df['t'] = self.create_df_time_axis(self.df)
+        #self.df['t'] = self.create_df_time_axis(self.svgplot.dfs[0])
 
     def create_df_U_axis(self, df):
         r'''
         Create voltage axis in the dataframe based on the units given in the
         figure description.
         '''
-        df_ = df.copy()
+        # df_ = df.copy()
         # Call a dict and remove the if functions
-        if self.xunit == 'V':
-            df_['U_V'] = df['U']
+        q = 1 * self.axis_units['x']
+        conversion_factor = q.to(u.V).value
+        df['U'] = df['x'] * conversion_factor
+        #self.axis_unit_strings['x']
+        #if self.xunit == 'V':
+        #    df_['U_V'] = df['U']
 
-        if self.xunit == 'mV':
-            df_['U_V'] = df['U']/1E3
+        #if self.xunit == 'mV':
+        #    df_['U_V'] = df['U']/1E3
 
-        df_['U_mV'] = df_['U_V']*1E3
-
-        return df_[['U_V', 'U_mV']]
+        #df_['U_mV'] = df_['U_V']*1E3
+        return df[['U']]
 
     def create_df_I_axis(self, df):
         r'''
         Create current or current density axis in the dataframe based on the
         units given in the figure description.
         '''
+        pass 
+        # Implement unit conversino
+        # q = 1 * self.axis_units['y']
+        # conversion_factor = q.to(u.A / u.m**2)
+
         df_ = df.copy()
         if self.yunit == 'A':
             df_['I_A'] = df['I']
@@ -92,10 +149,10 @@ class CV():
         figure description.
         '''
         df_ = df.copy()
-        df_['deltaU'] = abs(df_[self.xlabel].diff())
+        df_['deltaU'] = abs(df_['U'].diff())
         df_['cumdeltaU'] = df_['deltaU'].cumsum()
         df_['t'] = df_['cumdeltaU']/self.get_rate()
-        return df_['t']
+        return df_[['t']]
 
     def plot_cv(self):
         self.df.plot(x='U_V', y='I_uA')
