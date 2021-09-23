f55a4b45fceea7b7626c7f4f860e6e00f1995546
Julian Rüth <julian.rueth@fsfe.org>
2021-09-23 17:47:51 +0200

    Generate documentation with Sphinx

---
+++ doc/conf.py
@@ -0,0 +1,16 @@
+project = 'svgdigitizer'
+copyright = '2021, the svgdigitizer authors'
+author = 'the svgdigitizer authors'
+
+release = '0.0.0'
+
+
+extensions = []
+
+templates_path = ['_templates']
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'alabaster'
+
+html_static_path = []
