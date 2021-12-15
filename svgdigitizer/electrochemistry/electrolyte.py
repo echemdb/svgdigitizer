47a61c9eb1631f4a8d32a7f31bc0da3bd6e72009
Johannes Hermann <linuxrider@web.de>
2021-12-15 09:49:35 +0100

    integrated suggestions from #94

--- svgdigitizer/electrochemistry/electrolyte.py
+++ svgdigitizer/electrochemistry/electrolyte.py
@@ -1,3 +1,10 @@
+r"""
+This module contains the :class: `Electrolyte` which represents the electrolyte 
+with all it's components and handles everything related to it, such as
+composition, pH (aqueous only), etc. Furthermore, the dict `acid_base` contains
+a database of chemicals identified by InChI with their respective pKa values.
+
+"""
 # ********************************************************************
 #  This file is part of svgdigitizer.
 #
@@ -96,7 +103,15 @@ acid_base = {
 
 class Electrolyte:
     r"""
-    Electrolyte
+    :class: Electrolyte models the electrolyte i.e. the solution with all it's components.
+
+    EXAMPLES:
+
+    >>> from svgdigitizer.electrochemistry.electrolyte import Electrolyte
+    >>> electrolyte_dict = {"type": "aq", "components": [{"name": "H2SO4","type": "acid", "concentration": {"value": 0.1, "unit": "M"}}, {"name": "Na2SO4","type": "salt", "concentration": {"value": 0.1, "unit": "M"}}]}
+    >>> electrolyte = Electrolyte(electrolyte_dict)
+    >>> electrolyte.pH
+    1.3538681030273414
     """
 
     def __init__(self, electrolyte):
@@ -106,35 +121,50 @@ class Electrolyte:
     @cache
     def pH(self):
         r"""
-        pH of the electrolyte.
-        """
-        try:
-            return self.electrolyte["pH"]
-        except KeyError:
-            return self.estimate_pH()
+        pH of the electrolyte which is either taken from the electrolyte description or if not
+        present estimated using the pHcalc package.
+        
+        EXAMPLES:
 
-    def estimate_pH(self):
-        r"""
-        Estimates the pH of aquoues electrolytes.
-
-        EXAMPLES::
-
-            CID can be retrieved with InChIKey::
             >>> from svgdigitizer.electrochemistry.electrolyte import Electrolyte
+            >>> electrolyte_dict = {"pH": 1.0, "type": "aq", "components": [{"name": "H2SO4","type": "acid", "concentration": {"value": 0.1, "unit": "M"}}, {"name": "Na2SO4","type": "salt", "concentration": {"value": 0.1, "unit": "M"}}]}
+            >>> electrolyte = Electrolyte(electrolyte_dict)
+            >>> electrolyte.pH
+            1.0
+
             >>> electrolyte_dict = {"type": "aq", "components": [{"name": "H2SO4","type": "acid", "concentration": {"value": 0.1, "unit": "M"}}, {"name": "Na2SO4","type": "salt", "concentration": {"value": 0.1, "unit": "M"}}]}
             >>> electrolyte = Electrolyte(electrolyte_dict)
             >>> electrolyte.pH
             1.3538681030273414
+
+        """
+        if 'pH' in self.electrolyte: return self.electrolyte['pH']
+        return self._estimate_pH()
+
+
+    def _estimate_pH(self):
+        r"""
+        Return an estimate of the pH of this aquoues electrolytes.
+
+        EXAMPLES:
+
+            >>> from svgdigitizer.electrochemistry.electrolyte import Electrolyte
+            >>> electrolyte_dict = {"type": "aq", "components": [{"name": "H2SO4","type": "acid", "concentration": {"value": 0.1, "unit": "M"}}, {"name": "Na2SO4","type": "salt", "concentration": {"value": 0.1, "unit": "M"}}]}
+            >>> electrolyte = Electrolyte(electrolyte_dict)
+            >>> electrolyte._estimate_pH
+            1.3538681030273414
+        
         """
 
         if self.electrolyte["type"] != "aq":
-            raise NotImplementedError(
-                "pH calculations are only implemented for aqueous solutions!"
-            )
+            print("pH calculations are only implemented for aqueous solutions.")
+            return None
+
+
         acids_bases = []
         for i in self.electrolyte["components"]:
-            if not i["type"] == "solvent":
-                InChI = self.lookup_chemical_ids_from_name(i["name"])["InChI"]
+            if i["type"] != "solvent":
+                InChI = Electrolyte.lookup_chemical_ids_from_name(i["name"])
                 temp = self.get_pKa(InChI)
                 for e in temp:
                     q = i["concentration"]["value"] * normalize_unit(
@@ -149,29 +179,28 @@ class Electrolyte:
         return system.pH
 
     @classmethod
-    def lookup_CID_from_InChI(cls, InChI):
+    def get_CID_from_InChI(cls, InChI):
         r"""
         Lookup Pubchem CID from InChI.
 
-        EXAMPLES::
+        EXAMPLES:
 
-            CID can be retrieved with InChI::
-            >>> Electrolyte.lookup_CID_from_InChI(InChI='InChI=1S/H3O4P/c1-5(2,3)4/h(H3,1,2,3,4)') # phosphoric acid
+        The PubChem Compound ID (CID) can be retrieved with the International Chemical Identifier (InChI)::
+            >>> Electrolyte.get_CID_from_InChI(InChI='InChI=1S/H3O4P/c1-5(2,3)4/h(H3,1,2,3,4)') # phosphoric acid
             1004
 
         """
         return pcp.get_cids(InChI, "inchi")[0]
 
     @classmethod
-    def lookup_chemical_ids_from_name(cls, chemical_name):
+    def get_chemical_ids_from_name(cls, chemical_name):
         r"""
-        Lookup CID, InChI, and InChIKey from chemical name.
+        Get CID, InChI, and InChIKey from chemical name using the pubchempy package.
 
-        EXAMPLES::
+        EXAMPLES:
 
-            CID can be retrieved with InChIKey::
             >>> from svgdigitizer.electrochemistry.electrolyte import Electrolyte
-            >>> Electrolyte.lookup_chemical_ids_from_name('phosphoric acid')
+            >>> Electrolyte.get_chemical_ids_from_name('phosphoric acid')
             {'CID': 1004, 'InChI': 'InChI=1S/H3O4P/c1-5(2,3)4/h(H3,1,2,3,4)', 'InChIKey': 'NBIIXXVUZAFLBC-UHFFFAOYSA-N'}
 
         """
@@ -179,27 +208,28 @@ class Electrolyte:
         len_results = len(results)
         if len_results > 1:
             raise RuntimeError(
-                "Ambiguous chemical name! More than one chemical id return by pubchem. Use another chemical name"
+                "Ambiguous chemical name. More than one chemical id return by pubchem. Use another chemical name"
             )
         elif len_results == 0:
             raise RuntimeError(
-                f"Name of chemical {chemical_name} not found in pubchem! Check spelling."
+                f"Name of chemical {chemical_name} not found in pubchem. Check spelling."
             )
         else:
             return pcp.get_properties(("InChI", "InChIKey"), results[0])[0]
 
     def get_pKa(self, InChI):
         r"""
-        Get pKa
-        Pubchem lookup only works for neutral acids!
+        Get pKa of compound either from `acid_base` database in the `electrolyte` module or if not present there from 
+        pubchem. Due to the complexity of modeling compounds with classes provided by the pHcalc package pubchem lookup
+        works for neutral acids only.
 
         """
         pattern = re.compile(r".*(?P<pKa>\d+\.\d*).*")
         try:
             return acid_base[InChI]["components"]
         except KeyError:
-            cid = self.lookup_CID_from_InChI(InChI=InChI)
-            pKa_result = self.lookup_pKa(cid)["pKa"]
+            cid = self.get_CID_from_InChI(InChI=InChI)
+            pKa_result = self.get_pKa(cid)["pKa"]
             # TODO support salts etc.
             return [
                 Acid(
@@ -208,49 +238,35 @@ class Electrolyte:
                 )
             ]
 
-    def lookup_pKa(self, cid):
+    def get_pKa(self, cid):
         r"""
-        get url from Pubchem to get pka lookup result
+        get url from Pubchem to get pka  result
         'XML' can be replaced with 'JSON' but it is harder to parse later on
         for more info about Pubchem output types: https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest$_Toc494865558
-        stolen from https://github.com/khoivan88/pka_lookup/blob/master/src/pka_lookup_pubchem.py
+        copied from https://github.com/khoivan88/pka_lookup/blob/master/src/pka_lookup_pubchem.py which is MIT licensed.
         """
-        pka_lookup_result_xml = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/{cid}/XML?heading=Dissociation+Constants"
-        headers = {
-            "user-agent": "Mozilla/5.0 (X11; CentOS; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36"
-        }
+        from urllib.request import urlopen
+        response = urlopen(f'https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/{cid}/XML?heading=Dissociation+Constants')
+        tree = ET.fromstring(response.read())
 
-        # Get the html request info using CID number from pubchem
-        r = requests.get(pka_lookup_result_xml, headers=headers, timeout=15)
-        # Check to see if give OK status (200) and not redirect
-        if r.status_code == 200 and len(r.history) == 0:
-            # print(r.text)
-            # Use python XML to parse the return result
-            tree = ET.fromstring(r.text)
+        # Get the XML tree of <Information> only
+        info_node = tree.find(
+            ".//*{http://pubchem.ncbi.nlm.nih.gov/pug_view}Information"
+        )
 
-            # Get the XML tree of <Information> only
-            info_node = tree.find(
-                ".//*{http://pubchem.ncbi.nlm.nih.gov/pug_view}Information"
-            )
+        # Get the pKa reference:
+        original_source = info_node.find(
+            "{http://pubchem.ncbi.nlm.nih.gov/pug_view}Reference"
+        ).text
+        # Get the pKa result:
+        pka_string = info_node.find(
+            ".//*{http://pubchem.ncbi.nlm.nih.gov/pug_view}String"
+        ).text
+        pka_result = re.sub(
+            r"^pKa = ", "", pka_result
+        )  # remove 'pka = ' part out of the string answer
 
-            # Get the pKa reference:
-            original_source = info_node.find(
-                "{http://pubchem.ncbi.nlm.nih.gov/pug_view}Reference"
-            ).text
-            # Get the pKa result:
-            pka_result = info_node.find(
-                ".//*{http://pubchem.ncbi.nlm.nih.gov/pug_view}String"
-            ).text
-            pka_result = re.sub(
-                r"^pKa = ", "", pka_result
-            )  # remove 'pka = ' part out of the string answer
-
-            result = {
-                "pKa": pka_result,
-                "reference": original_source,
-            }
-
-            return result
+        return pka_result
 
         else:
             raise RuntimeError("pKa not found in Pubchem.")
