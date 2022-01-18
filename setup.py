67d1ec24a3acc7eb7b045d8b3f6ef5d8e35eacdf
Julian Rüth <julian.rueth@fsfe.org>
2022-01-17 22:44:37 -0600

    Add missing dependency to setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     install_requires = [
         'astropy>=5,<6',
         'click>=8,<9',
+        'datapackage>=1.15,<2',
         'matplotlib>=3.5,<4',
         'pandas>=1.3,<2',
         'pdf2image>=1.16,<2',
