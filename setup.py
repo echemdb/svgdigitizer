1dd464c3cdd06c9ad12053f7451161184ae1c451
Albert Engstfeld <albert.engstfeld@gmail.com>
2022-03-14 21:37:22 +0100

    Add frictionless to setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         "astropy>=5,<6",
         "click>=8,<9",
-        "datapackage>=1.15,<2",
+        "frictionless>=4,<5",
         "matplotlib>=3.5,<4",
         "pandas>=1.3,<2",
         "pdf2image>=1.16,<2",
