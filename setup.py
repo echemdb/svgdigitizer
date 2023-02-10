f0e6824355e8e23ed5ee3eec9e00c0e050d98496
Albert Engstfeld <albert.engstfeld@gmail.com>
2023-02-10 12:10:48 +0100

    change frictionless dependency

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         "astropy>=5,<6",
         "click>=8,<9",
-        "frictionless>=4.28.2,<5",
+        "frictionless>=5.5.0,<6",
         "matplotlib>=3.5,<4",
         "mergedeep>=1.3.4,<2",
         "pandas>=1.3,<2",
