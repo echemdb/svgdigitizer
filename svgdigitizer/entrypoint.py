6895921d90e8d02467bb31842fa2b65972b213dc
Albert Engstfeld <albert.engstfeld@gmail.com>
2023-03-08 16:42:57 +0100

    Fix CLI package creation

--- svgdigitizer/entrypoint.py
+++ svgdigitizer/entrypoint.py
@@ -311,7 +311,7 @@ def _create_package(metadata, csvname, outdir):
 
     # Update fields in the datapackage describing the data in the CSV
     package_schema = package.resources[0].schema
-    data_description_schema = Schema.from_descriptor(package.custom["data description"])
+    data_description_schema = Schema.from_descriptor({'fields':package.custom["data description"]["fields"]})
 
     new_fields = []
     for name in package_schema.field_names:
