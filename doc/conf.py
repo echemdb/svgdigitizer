66dd404c5f30b263f233f598db1a95a18ba431b1
Albert Engstfeld <albert.engstfeld@gmail.com>
2023-03-31 19:47:29 +0200

    bumped version to 0.9.0

--- doc/conf.py
+++ doc/conf.py
@@ -2,7 +2,7 @@ project = 'svgdigitizer'
 copyright = '2021, the svgdigitizer authors'
 author = 'the svgdigitizer authors'
 
-release = '0.8.0'
+release = '0.9.0'
 
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "myst_nb"]
