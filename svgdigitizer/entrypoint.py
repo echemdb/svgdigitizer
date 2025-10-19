debda30d06195c02e16917be9333ef7f0d6d453d
Albert Engstfeld <albert.engstfeld@gmail.com>
2025-10-18 20:36:19 -0500

    fix test files

--- svgdigitizer/entrypoint.py
+++ svgdigitizer/entrypoint.py
@@ -403,7 +403,9 @@ def _create_outfiles(svgfigure, svg, outdir, bibliography):
 
     metadata = svgfigure.metadata
 
-    if bibliography:
+    bibliography_data = _create_bibliography(bibliography, metadata)
+
+    if bibliography_data:
         metadata.setdefault("source", {})
         metadata["source"].setdefault("bibdata", {})
 
@@ -411,12 +413,8 @@ def _create_outfiles(svgfigure, svg, outdir, bibliography):
             logger.warning(
                 "The key with name `bibliography` in the metadata will be overwritten with the new bibliography data."
             )
-        bibliogrpaghy_data = _create_bibliography(bibliography, metadata)
 
-        if bibliogrpaghy_data:
-            metadata["source"].update(
-                {"bibdata": bibliogrpaghy_data}
-            )
+        metadata["source"].update({"bibdata": bibliogrpaghy_data})
 
     package = _create_package(metadata, csvname, outdir)
 
