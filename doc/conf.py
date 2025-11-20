cb67920284edf427af0805a56e567a7086a8615f
Albert Engstfeld <albert.engstfeld@gmail.com>
2025-11-20 10:36:45 +0100

    bumped version to 0.14.1

--- doc/conf.py
+++ doc/conf.py
@@ -2,7 +2,7 @@ project = 'svgdigitizer'
 copyright = '2021-2024, the svgdigitizer authors'
 author = 'the svgdigitizer authors'
 
-release = '0.14.0'
+release = '0.14.1'
 
 extensions = [
         "sphinx.ext.autodoc",
